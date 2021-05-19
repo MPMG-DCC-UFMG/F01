81e742639e88744e692226c7dac910e4d91f1a9f
asafeclemente <asafecgm1@gmail.com>
2021-05-19 19:22:36 -0300

    Check right links With RandomUnderSampler()

--- exploration/check_links/classifier_links.py
+++ exploration/check_links/classifier_links.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import size
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -5,14 +6,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.neighbors import KNeighborsClassifier
+from collections import Counter
+from sklearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from numpy import mean
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
 
 df = pd.read_csv("links.tsv", sep='\t')
 df['Link Correto'] = df['Link Correto'].replace(['x'], 1)
@@ -34,6 +38,9 @@ df.loc[:, "contas públicas"] = 0
 df.loc[:, "obras públicas"] = 0
 df.loc[:, "portal da transparência"] = 0
 df.loc[:, "transparência"] = 0
+# df.loc[:, "link transparência"] = 0 ######
+
+# df.loc[:, "portal no link"] = 0
 
 targets_for_text = ['plurianual', 'despesas', 'receitas', 'servidores', 'orçamentária', 'licitações', 'contratos', 'inexigibilidade',
                     'dispensa', 'concurso', 'contas públicas', 'obras públicas', 'portal da transparência', 'transparência']
@@ -61,6 +68,11 @@ def pontuar_os_municipios():
                 if portal_estadual:
                     df.loc[count, "consegui_html"] = None
 
+                # link_transp = re.findall('transparencia|tp|portal-da-transparencia|portal', df.loc[count, 'Portal da Transparência']) 
+                # for palavra in link_transp:
+                #     df.loc[count, "link transparência"] = +1
+                
+                # print(text)
                 find_targets = pontuar_texto(text)
                 for palavra in find_targets:
                     df.loc[count, palavra] += 1
@@ -70,11 +82,10 @@ def pontuar_os_municipios():
             continue
 
 
+def Decision_Tree_Classification( X_train, X_test, y_train, y_test):
 
-def Decision_Tree_Classification(X,y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.8, random_state = 0)
     classifier = DecisionTreeClassifier(criterion = 'gini', random_state = 0, min_samples_leaf = 5)
-    classifier.fit(X_train, y_train.ravel())
+    classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
     cm = confusion_matrix(y_test, y_pred)
     print(cm)
@@ -88,31 +99,46 @@ def Decision_Tree_Classification(X,y):
     fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=500)
     plot_tree(classifier,
             filled = True,
-            feature_names=fn);
-    fig.savefig('imagename.png')
-    data = df
-    data = data.drop(columns=['Site Prefeitura', 'Site Camara', 'Desenvolvedores', 'Portal da Transparência', 'Url do Link Correto'])
-    data = data.dropna(subset=['consegui_html'])
-    z_pred = classifier.predict(data.iloc[ :, 2:].values)
-    print (z_pred.tolist().count(1))
+            feature_names=fn,
+            class_names=True);
+    fig.savefig('imagename2.png')
+    # data = df
+    # data = data.dropna(subset=['consegui_html'])
+    # data = data.drop(columns=['Site Prefeitura', 'Site Camara', 'Desenvolvedores', 'Portal da Transparência', 'Url do Link Correto'])
+    # z_pred = classifier.predict(data.iloc[ :, 2:].values)
+    # print (z_pred.tolist().count(1))
 
+    return y_test, y_pred
 
 def classificar(dataset):
 
-    X = dataset.iloc[:, 2:].values
-    y = dataset.iloc[:, 1:2].values
-    dataset.to_csv("data.csv", index = False)
+    X = dataset.iloc[:, 2:]
+    y = dataset.iloc[:, 1]
 
-    Decision_Tree_Classification(X,y)   
+    # With ndersampling the model is getting worse 
+    # under = RandomUnderSampler()
+    # # fit and apply the transform
+    # X, y = under.fit_resample(X, y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.6, random_state = 1)
+
+    # print('X_train',X_train.shape)
+    # print('X_test',X_test.shape)
+    # print('y_train',y_train.shape)
+    # print('y_test',y_test.shape)
+
+    y_test, y_pred = Decision_Tree_Classification(X_train, X_test, y_train, y_test)
 
 
 def main():
     pontuar_os_municipios()
     dataset = df.loc[ : 301, :]
 
-    dataset = dataset.drop(columns=['Site Prefeitura', 'Site Camara', 'Desenvolvedores', 'Portal da Transparência', 'Url do Link Correto'])
     dataset = dataset.dropna(subset=['consegui_html'])
+    dataset = dataset.drop(columns=['Site Prefeitura', 'Site Camara', 'Desenvolvedores', 'Portal da Transparência', 'Url do Link Correto'])
+    print(dataset.shape)
 
+    dataset.to_csv("data.csv", index = False)
     classificar(dataset)
 
 main()
