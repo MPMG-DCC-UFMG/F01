9a84b77e28f43483efef065d8d6a9cb5548fd0de
ritarez <ritarezborges@gmail.com>
2021-05-20 21:25:08 -0300

    end to end solution to concursos and api/selenium extraction comparation

---
+++ exploration/end to end/concursos_publicos.py
@@ -0,0 +1,65 @@
+import codecs
+import os
+import requests
+import constant
+from bs4 import BeautifulSoup
+
+
+checklist_concursos = {
+    'registro': False,
+    'copia_digital':False,
+    'detalhes_andamento':False,
+    'recursos':False,
+    'resultado':False,
+    'nomeacao':False
+}
+
+def convert_url_to_dir(url): #still needs solution
+    #return url.replace(constant.URL, '')
+    return 'downloads/categoria/ConcursoPublico-EditalNo00120190022019e0032019.html'
+
+def validate_file(url):
+    try:
+        request = requests.get(url)
+        #print('Trying to reach file', url)
+        if request.status_code < 400:  
+            return True
+
+    except (requests.exceptions.ConnectionError, 
+            requests.exceptions.Timeout, 
+            requests.exceptions.TooManyRedirects, 
+            TimeoutError):
+        
+        print("Failed to connect to ", url)
+        return False
+
+def search_in_dump(markup, item, search):
+    
+    for div in markup.find_all('div', {"class": "list-group"}):
+        checklist_concursos['registro'] = True #Existe algum registro dos concursos públicos
+        link = constant.URL + div.find('a', href = True)['href']
+        for s in search:
+            if(s in div.find('h4').getText() and validate_file(link)):  
+                checklist_concursos[item] = True
+                return
+        
+
+def validate_items(url):
+    dir = convert_url_to_dir(url)
+    if os.path.exists('./Governador Valadares/' + dir):
+        html = BeautifulSoup(codecs.open('./Governador Valadares/' + dir, 'r', 'utf-8').read(),  "html.parser" )
+        for key in checklist_concursos.keys():
+            search_in_dump(html, key, constant.CHECKLIST_CONCURSO_SEARCH[key])
+            
+    print(checklist_concursos)
+
+
+def main():
+    file = codecs.open('Governador Valadares/home/home.html', 'r', 'utf-8')    
+    html = BeautifulSoup(file.read(),  "html.parser" )
+    
+    for elem in html.find_all(href=True):
+        for s in html, constant.CONCURSO_PUBLICO: 
+            if s in elem.getText() and validate_items(elem['href']): return True
+
+main()
---
+++ exploration/end to end/constant.py
@@ -0,0 +1,28 @@
+LEGISLACAO_MUNICIPAL = ['Legislação Municipal', 'Regulamentação Municipal']
+ORGANIZACAO = ['Estrutura Organizacional', 'estrutura organizacional', 'ESTRUTURA ORGANIZACIONAL', 'Organograma', 'Estrutura Administrativa']
+CONCURSO_PUBLICO = ['Concursos Públicos']
+DIARIA_VIAGEM = ['Despesas por Diárias']
+URL = 'https://transparencia.valadares.mg.gov.br/'
+
+
+#Concurso Publico
+CHECKLIST_CONCURSO_SEARCH = {
+    'registro': [''],
+    'copia_digital': ['Edital'],
+    'detalhes_andamento': ['COMUNICADO', 'Comunicado'],
+    'recursos': ['Recursos'],
+    'resultado':['Aprovados'],
+    'nomeacao': ['Nomeação']
+}
+#Reserva de Viagem
+CHECKLIST_VIAGEM_SEARCH = {
+    'publicacao_informacoes': [''],
+    'nome':['Nome', 'Favorecido'],
+    'cargo': ['Cargo', 'Atribuição'],
+    'destino': ['Destino'],
+    'atividade':['Descrição', 'Motivo', 'Atividade Empenhada'],
+    'periodo': ['Periodo'],
+    'num_diarias':['Número de Diárias'],
+    'valor_total':['Valor'],
+    'base_legal':['Base Legal']
+}
