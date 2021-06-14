4b2c3b23354ed541a40f2431a5f87cddb90cb6a1
Pedro Henrique Andrade <72928223+carbo6ufmg@users.noreply.github.com>
2021-06-14 11:55:46 -0300

    add text for Planos e Leis Orçamentárias

--- exploration/end to end/constant.py
+++ exploration/end to end/constant.py
@@ -42,3 +42,9 @@ CHECKLIST_CONTRATOS = {
     'desc_objeto' : ['Objeto'],
     'licitacao_origem' : ['Licitação'],
 }
+#Textos de Planos e Leis Orçamentárias
+LEIS_ORCAMENTARIAS = {
+    'PLANO_PLURIANUAL': ['PPA - Plano Plurianual','Plano Plurianual'],
+    'LEI_ORCAMENTARIA': ['Lei Orçamentária Anual','LOA - Lei Orçamentária Anual'],
+    'LEI_DIRETRIZES':['Lei de Diretrizes Orçamentárias','LDO - Lei de Diretrizes Orçamentárias']
+}
