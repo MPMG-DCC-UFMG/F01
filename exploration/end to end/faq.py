3a509919ffc9f0105429d7ae0b088a1b13cc83ac
ritarez <ritarezborges@gmail.com>
2021-06-14 10:21:11 -0300

    made explain of faq clearer

--- exploration/end to end/faq.py
+++ exploration/end to end/faq.py
@@ -13,14 +13,17 @@ def get_macro():
     return html.find(text = constant.FAQ, href=True)
 
 def predict(markup):
-    questions = markup.find_all(text= re.compile(r'[?]+$'))
-    return markup.find(text=constant.FAQ_SEARCH), questions
+    questions_by_t = set(markup.find_all(text= re.compile(r'([?]+$)')))
+     
+    for a in markup.find_all("a", {'id': re.compile(r'^pergunta')}):
+        questions_by_t.add(a.getText())
+    return markup.find(text=constant.FAQ_SEARCH), questions_by_t
 
 def explain(ans, macro, questions):
     if(ans is not None):
-        print("Foi encontrado no menu principal do portal um link que tinha como valor textual:", macro.getText())
-        print("Na página direcionada pelo link foi encontrado o seguinte título:", ans)
-        print("Foram encontradas", len(questions), "questões na página")
+        print("Foi encontrado no menu principal do portal um link que tinha como valor textual:", macro.getText(), "\nLink:", macro['href'])
+        print("\nNa página direcionada pelo link foi encontrado o seguinte título:", ans)
+        print("Foram encontradas", len(questions), "perguntas na página\n")
 
 def main():
     macro = get_macro()
@@ -29,7 +32,8 @@ def main():
     filename = './Governador Valadares/faq/perguntas_frequentes.html'
     markup = BeautifulSoup(codecs.open(filename, 'r', 'utf-8').read(),  "html.parser" )
     ans, questions = predict(markup)
-    print(ans is not None and questions is not None)
+    prediction = (ans is not None and questions is not None)
+    print("\nPrediction:", prediction, "\n")
     explain(ans, macro, questions)
 
 main()
