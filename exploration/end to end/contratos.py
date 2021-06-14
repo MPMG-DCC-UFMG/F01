f23a99017c57a4fc05d9572ff8c5f5e6352a0897
ritarez <ritarezborges@gmail.com>
2021-06-14 11:07:26 -0300

    explain function from 'contratos'

--- exploration/end to end/contratos.py
+++ exploration/end to end/contratos.py
@@ -11,49 +11,72 @@ def get_all_filenames_in_dir(dir):
         break
     return f
 
-#Still no way to link btn links to folders 
-def get_item_page(link):
-   #To-do
-   return './Governador Valadares/contratos-aditivos'
-
 #Tag item exists
 def get_macro():
     filename = './Governador Valadares/home/home.html'   
     file = codecs.open(filename, 'r', 'utf-8')
     html = BeautifulSoup(file.read(),  "html.parser" )
-    return html.find(text = constant.CONTRATOS, href=True)['href']
+    return html.find(text = constant.CONTRATOS, href=True)
 
 #Searches for button for sorting elements
 def checks_for_order(html):
     order = html.find(text = constant.ORDENAR)
-    return order is not None
+    return order
 
 #Searches page for table of elements 
 def validate_elements(html):
     table = html.find('table')
-    if table.find('summary'):
-        if table.find('summary').getText() in constant.CONTRATOS:
-            return table     
-    return False
+    return table.find('summary')            
 
 #iterates through all pages calling the function above
-def searches_for_elements(dir, filenames):
+def check_for_elements(dir, filenames):
     for f in filenames:
         html = BeautifulSoup(codecs.open(dir + '/' +  f, 'r', 'utf-8').read(),  "html.parser" )
-        elements = validate_elements(html)
-        if elements: return True
-    return False
+        table = validate_elements(html)
+        return table
+    return None
+
+
+def predict(table, order):
+    prediction = table.getText() in constant.CONTRATOS and order is not None
+    return prediction
+
+def explain(macro, table, order):
+    if(macro is None):
+        print("Não foi encontrado no menu principal do portal um link que possua como valor textual alguma das seguintes palavras chave:")
+        for fs in constant.CONTRATOS:
+            print(fs, ' ')
+        return 
+    
+    print("Foi encontrado no menu principal do portal um link que tinha como valor textual:", macro.getText(), "\nLink:", macro['href'])
+    if(table is None):    
+        print("\nNão foi encontrada nenhuma tabela com alguma  das seguintes palavras chave:")
+        for fs in constant.CONTRATOS:
+            print(fs, ' ')
+        return 
+
+    print("\nNa página direcionada pelo link foi encontrada uma tabela com o seguinte título:", table.getText())    
+    if(order is None):
+        print("Não é possível organizar os resultados por uma ordem cronológica\n")
+        return
+    print("É possível organizar os resultados por uma ordem cronológica\n")
+     
 
 def main():
     macro = get_macro()
-    if(not macro):  return 
+    if(not macro):  
+        prediction = False 
+    else:
+        dir = './Governador Valadares/contratos-aditivos'   
+        filenames = get_all_filenames_in_dir(dir)
+        base_file = codecs.open(dir + '/' +  filenames[0], 'r', 'utf-8')
 
-    dir = get_item_page(macro)   
-    filenames = get_all_filenames_in_dir(dir)
-    base_file = codecs.open(dir + '/' +  filenames[0], 'r', 'utf-8')
+        table = check_for_elements(dir, filenames) #If the searched elements are in the page
+        order = checks_for_order(BeautifulSoup(base_file.read(),  "html.parser" )) # If elements can be sorted
 
-    elements = searches_for_elements(dir, filenames) #If the searched elements are in the page
-    order = checks_for_order(BeautifulSoup(base_file.read(),  "html.parser" )) # If elements can be sorted
+        prediction = predict(table, order)
+    
+    print("\nPrediction:", prediction, "\n")
+    explain(macro, table, order)
 
-    print(elements, order)
 main()
--- exploration/end to end/faq.py
+++ exploration/end to end/faq.py
@@ -12,28 +12,48 @@ def get_macro():
     html = BeautifulSoup(file.read(),  "html.parser" )
     return html.find(text = constant.FAQ, href=True)
 
-def predict(markup):
+def get_title_and_questions(markup):
     questions_by_t = set(markup.find_all(text= re.compile(r'([?]+$)')))
      
     for a in markup.find_all("a", {'id': re.compile(r'^pergunta')}):
         questions_by_t.add(a.getText())
     return markup.find(text=constant.FAQ_SEARCH), questions_by_t
 
-def explain(ans, macro, questions):
-    if(ans is not None):
-        print("Foi encontrado no menu principal do portal um link que tinha como valor textual:", macro.getText(), "\nLink:", macro['href'])
-        print("\nNa página direcionada pelo link foi encontrado o seguinte título:", ans)
-        print("Foram encontradas", len(questions), "perguntas na página\n")
+def predict(title, questions):
+    prediction = (title is not None and questions is not None)    
+    return prediction
+
+def explain(macro, title, questions):
+    if(macro is None):
+        print("Não foi encontrado no menu principal do portal um link que possua como valor textual alguma das seguintes palavras chave:")
+        for fs in constant.FAQ:
+            print(fs, ' ')
+        return  
+    
+    print("Foi encontrado no menu principal do portal um link que tinha como valor textual:", macro.getText(), "\nLink:", macro['href'])
+    if(title is None):    
+        print("\nNenhuma das palavras chave a seguir foram encontradas na página direcionada pelo link:")
+        for fs in constant.FAQ_SEARCH:
+            print(fs, ' ')
+        return 
+
+    print("\nNa página direcionada pelo link foi encontrado o seguinte título:", title)    
+    print("Foram encontradas", len(questions), "perguntas na página\n")
 
 def main():
     macro = get_macro()
-    if(not macro):  return 
-
-    filename = './Governador Valadares/faq/perguntas_frequentes.html'
-    markup = BeautifulSoup(codecs.open(filename, 'r', 'utf-8').read(),  "html.parser" )
-    ans, questions = predict(markup)
-    prediction = (ans is not None and questions is not None)
+    title = None
+    questions = None
+    if(not macro):  
+        prediction = False
+    else: 
+        filename = './Governador Valadares/faq/perguntas_frequentes.html'
+        markup = BeautifulSoup(codecs.open(filename, 'r', 'utf-8').read(),  "html.parser" )
+        title, questions = get_title_and_questions(markup)
+        prediction = predict(title, questions)
+    
     print("\nPrediction:", prediction, "\n")
-    explain(ans, macro, questions)
+
+    explain(macro, title, questions)
 
 main()
