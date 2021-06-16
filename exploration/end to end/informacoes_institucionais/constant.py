fa26dee1eb4530e87ef6019912f853ca80f5fd2f
Pedro Henrique Andrade <72928223+carbo6ufmg@users.noreply.github.com>
2021-06-16 08:41:09 -0300

    add constant

--- exploration/end to end/informacoes_institucionais/constant.py
+++ exploration/end to end/informacoes_institucionais/constant.py
@@ -1,3 +1,9 @@
 URL = 'https://transparencia.valadares.mg.gov.br/'
 LEGISLACAO_MUNICIPAL = ['Legislação Municipal', 'Regulamentação Municipal']
 ORGANIZACAO = ['Estrutura Organizacional', 'estrutura organizacional', 'ESTRUTURA ORGANIZACIONAL', 'Organograma', 'Estrutura Administrativa']
+#Textos de Planos e Leis Orçamentárias
+LEIS_ORCAMENTARIAS = {
+    'PLANO_PLURIANUAL': ['PPA - Plano Plurianual','Plano Plurianual'],
+    'LEI_ORCAMENTARIA': ['Lei Orçamentária Anual','LOA - Lei Orçamentária Anual'],
+    'LEI_DIRETRIZES':['Lei de Diretrizes Orçamentárias','LDO - Lei de Diretrizes Orçamentárias']
+}
