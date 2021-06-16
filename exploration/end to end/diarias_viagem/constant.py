7f79424684c8757a9214f5b468977052d78818bb
ritarez <ritarezborges@gmail.com>
2021-06-16 10:59:30 -0300

    added explain do 'diarias de viagem'

--- exploration/end to end/diarias_viagem/constant.py
+++ exploration/end to end/diarias_viagem/constant.py
@@ -3,7 +3,7 @@ DIARIA_VIAGEM = ['Despesas por Diárias']
 
 #Reserva de Viagem
 CHECKLIST_VIAGEM_SEARCH = {
-    'publicacao_informacoes': [''],
+    'publicacao_informacoes': ['Diárias', 'Despesas por Diárias', 'Despesas com Diárias'],
     'nome':['Nome', 'Favorecido'],
     'cargo': ['Cargo', 'Atribuição'],
     'destino': ['Destino'],
--- exploration/end to end/diarias_viagem/diaria_viagem.py
+++ exploration/end to end/diarias_viagem/diaria_viagem.py
@@ -24,11 +24,15 @@ def convert_url_to_dir(url): #still needs solution
 def validate_item(headers, key, search):
     for s in search: 
         if next((header for header in headers if s in header.getText()), None): 
-            checklist_viagens[key] = True
+            checklist_viagens[key] = s
 
 #Search for checklist atributes in correct page  
 def search_in_dump(markup):
     headers = markup.find('thead').find_all('th')
+    if(markup.find('table').find('summary')):
+        table_summary = markup.find('table').find('summary').getText() 
+        if(table_summary in constant.CHECKLIST_VIAGEM_SEARCH['publicacao_informacoes']): checklist_viagens['publicacao_informacoes'] = table_summary
+
     if markup.find('tbody').find_all('tr'): #if theres any rows in table
         for key in checklist_viagens.keys():
             validate_item(headers, key, constant.CHECKLIST_VIAGEM_SEARCH[key])
@@ -43,19 +47,29 @@ def get_all_filenames_in_dir(dir):
     return f
 
 #iterates through files 
-def validate_items(url):
+def predict(url):
     dir = './Governador Valadares/' + convert_url_to_dir(url)
     if os.path.exists(dir):
         filenames = get_all_filenames_in_dir(dir)
         html = BeautifulSoup(codecs.open('./Governador Valadares/' + dir + filenames[0], 'r', 'utf-8').read(),  "html.parser" )
         search_in_dump(html)    
 
+def explain():
+   print("\n")
+   for key in checklist_viagens.keys():
+        if checklist_viagens[key]:
+           print("Foi encontrada a seguinte coluna para", key, ":", checklist_viagens[key])
+        else: 
+           print("Não foi encontrada nenhuma referência a", key)
+   print("\n")
+
+
 def main():
     file = codecs.open('Governador Valadares/home/home.html', 'r', 'utf-8')    
     html = BeautifulSoup(file.read(),  "html.parser" )
     for elem in html.find_all(href=True):
         for s in constant.DIARIA_VIAGEM: 
-            if s in elem.getText() and validate_items(elem['href']): return True
+            if s in elem.getText() and predict(elem['href']): return True
 
-    print(checklist_viagens)
+    explain()
 main()
