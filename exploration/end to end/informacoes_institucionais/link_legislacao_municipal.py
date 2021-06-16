a2be8c217d504e0875357b1c11369946634f1cec
ritarez <ritarezborges@gmail.com>
2021-06-16 09:39:21 -0300

    added explain to 'link legislacao municiapal'

--- exploration/end to end/informacoes_institucionais/link_legislacao_municipal.py
+++ exploration/end to end/informacoes_institucionais/link_legislacao_municipal.py
@@ -4,30 +4,43 @@ import requests
 import constant
 from bs4 import BeautifulSoup
 
+def explain(title, url):
+    if(title):
+        if (constant.URL not in url): url = constant.URL + url
+        print("Na home do portal foi encontrado um link com o seguinte valor textual:", title)
+        print("O link", url, "é válido")
+    else: 
+        print("Não foi encontrada nenhuma das seguintes palavras chave com links validos:")
+        for w in constant.LEGISLACAO_MUNICIPAL:
+            print(w)
+
+
 def validate_item(url):
     dir = url.replace(constant.URL, '')
     if os.path.exists('./Governador Valadares/' + dir):
         return True
     try:
         request = requests.get(url)
-        print('Trying to reach ', url)
+        # print('Trying to reach ', url)
         if request.status_code < 400:  return True
 
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, TimeoutError):
         print("Failed to connect to ", url)
 
-def search_checklist_item(markup, search):
+def predict(markup, search):
 
     for elem in markup.find_all(href=True):
         for s in search: 
-            if s in elem.getText() and validate_item(elem['href']): return True
-
+            if s in elem.getText() and validate_item(elem['href']): 
+                explain(s, elem['href'])
+                return True
+    explain(None, None)
     return False
        
 
 def main():
     file = codecs.open('Governador Valadares/home/home.html', 'r', 'utf-8')    
     html = BeautifulSoup(file.read(),  "html.parser" )
-    print(search_checklist_item(html, constant.LEGISLACAO_MUNICIPAL))
+    print(predict(html, constant.LEGISLACAO_MUNICIPAL))
 
 main()
