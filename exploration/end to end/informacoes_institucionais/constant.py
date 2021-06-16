5e815d94cf410b678137b9594de1d50650ce61e2
Pedro Henrique Andrade <72928223+carbo6ufmg@users.noreply.github.com>
2021-06-16 08:42:59 -0300

    Update constant.py

--- exploration/end to end/informacoes_institucionais/constant.py
+++ exploration/end to end/informacoes_institucionais/constant.py
@@ -1,4 +1,3 @@
 URL = 'https://transparencia.valadares.mg.gov.br/'
 LEGISLACAO_MUNICIPAL = ['Legislação Municipal', 'Regulamentação Municipal']
 ORGANIZACAO = ['Estrutura Organizacional', 'estrutura organizacional', 'ESTRUTURA ORGANIZACIONAL', 'Organograma', 'Estrutura Administrativa']
-#Textos de Planos e Leis Orçamentárias
