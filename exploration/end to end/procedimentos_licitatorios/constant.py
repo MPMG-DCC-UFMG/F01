794ebcea0fdf994026fd23cff56667a72599f88b
asafeclemente <asafecgm1@gmail.com>
2021-06-22 12:52:07 -0300

    Licitações

--- exploration/end to end/procedimentos_licitatorios/constant.py
+++ exploration/end to end/procedimentos_licitatorios/constant.py
@@ -1,5 +1,19 @@
 URL = 'https://transparencia.valadares.mg.gov.br/'
 CONTRATOS = ['Contratos e Aditivos', 'Contratos', 'CONTRATOS', 'contratos']
+LICITACOES = ['Licitações', 'Licitacoes']
+
+ORDEM_LICITACOES = {
+    'registro': ['status', 'andamento', 'concluida'],
+    'situacao' : ['Anulada','Cancelada','Deserta','Em andamento','Encerrada','Fracassada','Homologada', 'Revogada', 'Suspensa' ],
+    'ordem' : ['ordenar por', 'data crescente', 'data decrescente', 'número crescente', 'número descente', 'data de abertura', 'data de publicação', 'data de publicações'],
+    'tipo' : ['Adesão Registro de Preço','Carta Convite','Chamada Pública','Concorrência','Concurso','Dispensa','Inexigibilidade','Leilão','Pregão','Pregão Eletrônico','Pregão Presencial','Regime Diferenciado de Contratações Públicas','Tomada de Preços']
+}
+
+
+OBJETO_LICITACOES = ['obejto', 'detalhamento do objeto']
+
+SITUACAO_LICITACOES = ['anulada', 'cancelada', 'deserta', 'andamento', 'encerrada', 'fracassada', 'homologada', 'revogada', 'suspensa' ]
+
 
 #Legislação
 CHECKLIST_CONTRATOS = {
