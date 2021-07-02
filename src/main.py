cde86fe5c6c5f8782f2d12de3c3682e361293314
Pedro Henrique Andrade <72928223+carbo6ufmg@users.noreply.github.com>
2021-07-02 09:21:09 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ sys.path.insert(1, './classifiers')
 from faq import predict_faq, explain_faq
 from concursos import predict_copia_edital, explain_copia_edital, predict_recursos, explain_recursos
 from diaria_viagem import predict_diaria_viagem, explain_diaria_viagem
+from info_institucionais import predict_estrutura_organizacional,explain_estrutura_organizacional,predict_link_legislacao,explain_link_legislacao,predict_unidades_administrativas,explain_unidades_administrativas
 
 if __name__ == "__main__":
 
@@ -20,6 +21,13 @@ if __name__ == "__main__":
 
     df = predict_diaria_viagem()
     explain_diaria_viagem(df)
+    
+    estrutura_bool = predict_estrutura_organizacional()
+    explain_estrutura_organizacional()
 
+    link_legislacao_bool = predict_link_legislacao()
+    explain_link_legislacao()
 
+    unidades_dict = predict_unidades_administrativas()
+    explain_unidades_administrativas()
 
