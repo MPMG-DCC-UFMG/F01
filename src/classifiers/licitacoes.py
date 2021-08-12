aedb50087fa3884f2d94ec8a40236345cec7d956
CinthiaS <cinthiamikaela.s@gmail.com>
2021-08-12 10:37:55 -0300

    Code Standardization

--- src/classifiers/licitacoes.py
+++ src/classifiers/licitacoes.py
@@ -5,7 +5,9 @@ import re
 import os
 import numpy as np
 import sys
+
 sys.path.insert(0, '../')
+#sys.path.insert(0, '/home/cinthia/MPMG/F01/src')
 
 from utils import indexing
 from utils import table_to_csv
@@ -72,20 +74,6 @@ def analyze_edital(df, column_name):
     else:
         return False
 
-
-def predict(df, column_name, threshold = 0): 
-    
-    if sum(df[column_name]) > threshold:
-        return True
-    else:
-        return False
-
-def explain(df, column_name):
-
-    print("Quantidade de arquivos analizados: {}\n Quantidade de aquivos válidos {}\n".format(
-         len(df[column_name]), sum(df[column_name])))
-
-
 def check_all_files_busca(paths, path_base, result):
 
     for path, type in paths:
@@ -97,8 +85,132 @@ def check_all_files_busca(paths, path_base, result):
 
     return result
 
+def check_result(df, column_name, threshold=0): 
+    
+    if sum(df[column_name]) > threshold:
+        return True
+    else:
+        return False
 
-def main (path_base):
+def preprocess_paths(sorted_result, word):
+
+    paths = search_path_in_dump.get_paths(sorted_result)
+    paths = (sorted(set(paths)))
+    paths = search_path_in_dump.filter_paths(paths, word)
+
+    return paths
+
+def predict_proc_lic(
+    search_term, keywords_search, path_base, num_matches=40,
+    keywords_check=['nmero da licitao', 'modalidade', 'objeto', 'status', 'editais'],
+    filter_word='licitacoes', job_name='index_gv', threshold = 0): 
+
+    result = {'proc_lic': []}
+
+    #Search
+    _, sorted_result = indexing.request_search(
+      search_term=search_term, keywords=keywords_search, num_matches=num_matches, job_name=job_name)
+    paths = preprocess_paths(sorted_result, word=filter_word)
+
+    #Convert
+    if os.path.isfile("licitacoes.csv"):
+        df = pd.read_csv("licitacoes.csv")
+    else:
+        df = search_path_in_dump.list_to_csv(paths, path_base, "licitacoes.csv")
+
+    #Analyze
+    result['proc_lic'] = analyze_proc_lici(df, keywords_check)
+
+    #Check
+    isvalid = []
+    for i in keywords_check:
+        isvalid.append(check_result(result['proc_lic'], i, threshold=threshold))
+
+    return isvalid, result
+
+
+def predict_inexigibilidade(
+    search_term, keywords_search, path_base, num_matches=40,
+     filter_word='licitacoes', job_name='index_gv', threshold=0): 
+
+    result = {'inexigibilidade': []}
+
+    #Search
+    _, sorted_result = indexing.request_search(
+      search_term=search_term, keywords=keywords_search, num_matches=num_matches, job_name=job_name)
+    paths = preprocess_paths(sorted_result, word=filter_word)
+
+    #Convert
+    if os.path.isfile("licitacoes.csv"):
+        df = pd.read_csv("licitacoes.csv")
+    else:
+        df = search_path_in_dump.list_to_csv(paths, path_base, "licitacoes.csv")
+
+    #Analyze
+    isvalid, column_modalidade = search_path_in_dump.check_columns(df, word='modalidade')
+    if isvalid:
+        for index, value in df.iterrows():
+            result['inexigibilidade'].append(analyze_inexibilidade (value, column_modalidade))
+    
+    #Check
+    isvalid = check_result(result, column_name='inexigibilidade', threshold=threshold)
+    
+    return isvalid, result
+
+def predict_editais(
+    search_term, keywords_search, path_base, num_matches=40,
+    filter_word='licitacoes', job_name='index_gv', threshold=0): 
+
+    result = {'editais': []}
+
+    #Search
+    _, sorted_result = indexing.request_search(
+      search_term=search_term, keywords=keywords_search, num_matches=num_matches, job_name=job_name)
+    paths = preprocess_paths(sorted_result, word=filter_word)
+
+    #Convert
+    if os.path.isfile("licitacoes.csv"):
+        df = pd.read_csv("licitacoes.csv")
+    else:
+        df = search_path_in_dump.list_to_csv(paths, path_base, "licitacoes.csv")
+
+    #Analyze
+    isvalid, column_edital = search_path_in_dump.check_columns(df, word='editais')
+    if isvalid:
+        for index, value in df.iterrows():
+            result['editais'].append(analyze_edital(df, column_edital))
+
+    #Check
+    isvalid = check_result(result, column_name='editais', threshold=threshold)
+    
+    return isvalid, result
+
+def predict_busca(
+    search_term, keywords_search, path_base, num_matches=40,
+    filter_word='licitacoes', job_name='index_gv', threshold=0): 
+
+    result = {'busca': []}
+
+    #Search
+    _, sorted_result = indexing.request_search(
+      search_term=search_term, keywords=keywords_search, num_matches=num_matches, job_name=job_name)
+    paths = preprocess_paths(sorted_result, word=filter_word)
+
+    #Analyze
+    result = check_all_files_busca(paths, path_base, result)
+
+    #Check
+    isvalid = check_result(result, column_name='busca', threshold=threshold)
+    
+    return isvalid, result
+
+def explain(df, column_name):
+
+    print("Explain - Quantidade de arquivos analizados: {}\n\tQuantidade de aquivos válidos: {}\n".format(
+         len(df[column_name]), sum(df[column_name])))
+
+
+"""def main (path_base):
 
     search_term="Licitações"
     keywords = ["Licitações", "Pregão", "Inexigibilidade", "Homologada", "Resultado Final de Licitação", "Modalidade", "Fundamentação legal", "Status" ]
@@ -144,11 +256,47 @@ def main (path_base):
     #Busca
     result = check_all_files_busca(paths, path_base, result)
     print("Permite Busca: {}".format(predict(result, 'busca', threshold = 0)))
-    print(explain(result, 'busca'))
+    print(explain(result, 'busca'))"""
 
 
-#local onde está o dump
+"""#local onde está o dump
 path_base = "/home/cinthia/MPMG/persistence_area"
 # path_base = "C:/Users/ritar"
 
-main (path_base)
+#main (path_base)
+
+search_term="Licitações"
+keywords_search = ["Licitações", "Pregão", "Inexigibilidade", "Homologada", "Resultado Final de Licitação", "Modalidade", "Fundamentação legal", "Status" ]
+
+keywords_check=['nmero da licitao', 'modalidade', 'objeto', 'status', 'editais']
+
+isvalid, result = predict_proc_lic(
+    search_term, keywords_search, path_base, num_matches=40,
+    keywords_check=keywords_check,
+    filter_word='licitacoes', job_name='index_gv', threshold = 0)
+
+
+for i in range(len(keywords_check)):
+  print("Predict - Procedimentos Licitatórios - {}: {}".format(keywords_check[i], isvalid[i]))
+  print(explain(result['proc_lic'], keywords_check[i]))
+
+isvalid, result = predict_inexigibilidade(
+    search_term, keywords_search, path_base, num_matches=40,
+     filter_word='licitacoes', job_name='index_gv', threshold=0)
+
+print("Predict - Inexigibilidade: {}".format(isvalid))
+print(explain(result, 'inexigibilidade'))
+
+isvalid, result = predict_editais(
+    search_term, keywords_search, path_base, num_matches=40,
+    filter_word='licitacoes', job_name='index_gv', threshold=0)
+
+print("Predict - Editais: {}".format(isvalid))
+print(explain(result, 'editais'))
+
+isvalid, result = predict_busca(
+    search_term, keywords_search, path_base, num_matches=40,
+    filter_word='licitacoes', job_name='index_gv', threshold=0)
+
+print("Predict - Busca: {}".format(isvalid))
+print(explain(result, 'busca'))"""
--- src/main.py
+++ src/main.py
@@ -1,17 +1,62 @@
 import sys
 sys.path.insert(1, './classifiers')
 
+#sys.path.insert(0, '/home/cinthia/MPMG/F01/src/classifiers')
 
 from concursos import predict_copia_edital, explain_copia_edital, predict_recursos, explain_recursos, predict_dados_concurso, explain_dados_concurso
 from diaria_viagem import predict_diaria_viagem, explain_diaria_viagem
 from info_institucionais import predict_estrutura_organizacional,explain_estrutura_organizacional,predict_link_legislacao,explain_link_legislacao,predict_unidades_administrativas,explain_unidades_administrativas
 from acesso_a_informacao.informacoes import predict_informacoes, explain_informacoes
-# from licitacoes import predict_licitacoes, explain_licitacoes
+import licitacoes 
 
 if __name__ == "__main__":
 
-    concursos_dict = predict_dados_concurso()
-    explain_dados_concurso(concursos_dict)
+    #concursos_dict = predict_dados_concurso()
+    #explain_dados_concurso(concursos_dict)
+
+
+    
+    #local onde está o dump
+    path_base = "/home/cinthia/MPMG/persistence_area"
+    # path_base = "C:/Users/ritar"
+
+    search_term="Licitações"
+    keywords_search = ["Licitações", "Pregão", "Inexigibilidade", "Homologada", "Resultado Final de Licitação", "Modalidade", "Fundamentação legal", "Status" ]
+    
+    # Procedimentos licitatórios
+    keywords_check=['nmero da licitao', 'modalidade', 'objeto', 'status', 'editais']
+    isvalid, result = licitacoes.predict_proc_lic(
+        search_term, keywords_search, path_base, num_matches=40,
+        keywords_check=keywords_check,
+        filter_word='licitacoes', job_name='index_gv', threshold = 0)
+
+    for i in range(len(keywords_check)):
+        print("Predict - Procedimentos Licitatórios - {}: {}".format(keywords_check[i], isvalid[i]))
+        print(licitacoes.explain(result['proc_lic'], keywords_check[i]))
+
+    # Procedimentos de Inexigibilidade
+    isvalid, result = licitacoes. predict_inexigibilidade(
+        search_term, keywords_search, path_base, num_matches=40,
+        filter_word='licitacoes', job_name='index_gv', threshold=0)
+
+    print("Predict - Inexigibilidade: {}".format(isvalid))
+    print(licitacoes.explain(result, 'inexigibilidade'))
+
+    # Disponibilização de Editais
+    isvalid, result = licitacoes.predict_editais(
+        search_term, keywords_search, path_base, num_matches=40,
+        filter_word='licitacoes', job_name='index_gv', threshold=0)
+
+    print("Predict - Editais: {}".format(isvalid))
+    print(licitacoes.explain(result, 'editais'))
+
+    # Permite Busca
+    isvalid, result = licitacoes.predict_busca(
+        search_term, keywords_search, path_base, num_matches=40,
+        filter_word='licitacoes', job_name='index_gv', threshold=0)
+
+    print("Predict - Busca: {}".format(isvalid))
+    print(licitacoes.explain(result, 'busca'))
 
     # concursos_dict = predict_copia_edital()
     # explain_copia_edital(concursos_dict)
