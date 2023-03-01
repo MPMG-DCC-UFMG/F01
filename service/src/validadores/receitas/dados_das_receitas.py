59b58926e4d09ef63d11aff43e91e48b8f510ab1
jorgesilva2407 <jorge.lima2407@gmail.com>
2023-03-01 20:59:24 -0300

    Receitas - Dados de Receitas - PT (45)

--- service/src/validadores/receitas/dados_das_receitas.py
+++ service/src/validadores/receitas/dados_das_receitas.py
@@ -11,7 +11,14 @@ class ValidadorDadosDasReceitas:
         files = indexing.get_files(keywords['search_term'], job_name, keywords_search=keywords['keywords_to_search'])
         files = path_functions.filter_paths(files, words=['receitas'])
         self.files = path_functions.agg_paths_by_type(files)
-        self.df = get_df(self.files, keywords['types'])
+        try:
+            max_files = keywords['max_files']
+        except KeyError:
+            max_files = 5
+
+        print(len(self.files['html']))
+        self.df = get_df(self.files, keywords['types'], max_files=max_files)
+        self.df.to_csv('aux.csv')
 
     # Previsão
     def predict_previsao(self):
@@ -58,11 +65,5 @@ class ValidadorDadosDasReceitas:
         return resultados_dados_das_receitas
         
     def explain(self, result, column_name, description):
-        try:
-            numero_de_entradas = len(result[column_name])
-        except KeyError:
-            numero_de_entradas = 0
-
-        result = f"""Quantidade de entradas encontradas e analizadas: {numero_de_entradas}.
-        Quantidade de entradas que possuem o campo {description} da receita válido: {sum(result['isvalid'])}"""
+        result = f"""Quantidade de entradas que possuem o campo {description} da receita válido: {sum(result['isvalid'])}"""
         return result
