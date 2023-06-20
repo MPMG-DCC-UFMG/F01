745b53d9509d22e0c3c6f474cee02da09ba66e22
asafeclemente <asafecgm1@gmail.com>
2023-06-20 12:29:41 -0300

    Documentação Indexação de Arquivos

--- service/src/api_de_integracao/views.py
+++ service/src/api_de_integracao/views.py
@@ -14,13 +14,29 @@ from src.api_de_integracao.resposta import Resposta
 
 api_de_integracao = Blueprint('api_de_integracao', __name__)
 
-
-# Indexar todos os arquivos de um template no elasticsearch usando o fscrawler
-#  Executa o script para indexar arquivos de um template específico no Elasticsearch usando o fscrawler.
 @api_de_integracao.route('/indexar_arquivos/<string:nome_do_template>', methods=['GET'])
 def indexarArquivosTemplate(nome_do_template):
     """Indexar arquivos pelo nome do template
-    Este endpoint permite indexar arquivos com base no nome do template.
+    Este endpoint permite indexar arquivos com base no nome do template. Executa o script para indexar arquivos de um template específico no **Elasticsearch** usando o **fscrawler**.
+    
+    Esse código executa uma função chamada scrip_indexar_arquivos_mp que tem a finalidade de indexar arquivos de um template específico no **Elasticsearch** usando o **fscrawler**. Vou explicar as partes principais do código:
+
+    1. O código obtém uma lista de municípios relacionados ao template fornecido usando a função `get_municipios_do_template(nome_do_template)`. Os municípios são armazenados na variável `MUNICIPIOS`.
+    
+    2. O código entra em um loop que percorre cada município da lista `MUNICIPIOS`.
+    
+    3. Em seguida, o código define um dicionário chamado ``config`` que contém várias configurações relacionadas à indexação de arquivos usando o **fscrawler**. As configurações incluem informações sobre o local dos arquivos a serem indexados, configurações do **Elasticsearch** e configurações específicas do **fscrawler**.
+    
+    4. O código verifica se o diretório *municipio_directory* já existe. Se existir, verifica se um arquivo de configuração `_settings.yaml` também existe. Se o arquivo de configuração existir, exibe uma mensagem informando que o arquivo de configuração já existe. Caso contrário, o código cria o arquivo de configuração e o preenche com as configurações contidas no dicionário ``config``.
+    
+    5. Se o diretório *municipio_directory* não existir, o código cria o diretório e em seguida cria o arquivo de configuração `_settings.yaml`, preenchendo-o com as configurações contidas no dicionário ``config``.
+    
+    6. Após a criação do arquivo de configuração, o código executa o **fscrawler** para iniciar o processo de indexação dos arquivos. Isso é feito usando a biblioteca *subprocess* para executar o comando do **fscrawler** como um processo separado.
+    
+    7. O código captura a saída do processo do **fscrawler** e exibe mensagens indicando se o processo foi concluído com êxito ou se foi encerrado devido a um limite de tempo *(TIME_OUT)*.
+    
+    8. Por fim, o código verifica se um arquivo `_status.json` foi gerado durante o processo de indexação. Se o arquivo existir, ele é aberto e o seu conteúdo é exibido.
+
     ---
     tags:
       - Indexação de Arquivos
