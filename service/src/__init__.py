787f3b2298805888709670d9aa7b82cf8ae8ca4f
asafeclemente <asafecgm1@gmail.com>
2023-07-04 11:11:32 -0300

    Removendo api de resultados da api de controle de validação

--- service/src/__init__.py
+++ service/src/__init__.py
@@ -39,12 +39,10 @@ from src.api_de_integracao.views import api_de_integracao
 from src.validadores.views import validadores
 from src.municipio.views import municipio
 from src.empresa.views import empresa
-from src.api_de_resultados.views import api_de_resultados
 from src.checklist.views import checklist
 
 app.register_blueprint(api_de_integracao)
 app.register_blueprint(validadores, url_prefix="/validadores")
-app.register_blueprint(api_de_resultados, url_prefix="/api")
 app.register_blueprint(municipio, url_prefix="/municipio")
 app.register_blueprint(empresa, url_prefix="/empresa")
 app.register_blueprint(checklist, url_prefix="/checklist")
